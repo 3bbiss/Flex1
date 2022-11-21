3f6c08f9e5912408c68c25e6d12121b0cdc1335a
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-11-21 13:23:40 -0500

    flex practice 1 to help with capstone

---
+++ RepairShopAPI/Models/Instrument.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace RepairShopAPI
+{
+    [Table("instrument")]
+    public class Instrument
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ RepairShopAPI/Models/RepairOrder.cs
@@ -0,0 +1,51 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace RepairShopAPI
+{
+    [Table("repairorder")]
+    public class RepairOrder
+    {
+        [Key]
+        public int id { get; set; }
+        public string customer { get; set; }
+        public int instrument_id { get; set; }
+        public int status { get; set; }
+        public decimal price { get; set; }
+        public string notes { get; set; }
+        public bool bookmark { get; set; }
+
+
+        // Static DAL members
+
+        // GetOne C(R)UD
+        public static RepairOrder GetOne(int id)
+        {
+            return DAL.DB.Get<RepairOrder>(id);
+        }
+
+
+        // Create (C)RUD
+        public static RepairOrder Add(RepairOrder order)
+        {
+            DAL.DB.Insert(order);
+            return order;
+        }
+
+        // Delete CRU(D)
+        public static void Delete(int id)
+        {
+            //DAL.DB.Delete(new RepairOrder { id = id }); // shorter
+
+            RepairOrder order = new RepairOrder();
+            order.id = id;
+            DAL.DB.Delete<RepairOrder>(order);
+        }
+
+        // Update CR(U)D
+        public static void Update(RepairOrder order)
+        {
+            DAL.DB.Update(order);
+        }
+
+    }
+}
