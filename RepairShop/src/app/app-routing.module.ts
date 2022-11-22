303105327bd06b7aa671359b881244a2d1fca1c7
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-11-22 16:00:49 -0500

    routing w bookmarks

--- RepairShop/src/app/app-routing.module.ts
+++ RepairShop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: OrderListComponent},
+{path: 'bookmarks', component: BookmarksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
